70e5d8ef47d6508e8e018f01fba2c02e4218d69f
Anurag Tiwari <anuragtiwarigpj2005@gmail.com>
2024-10-12 23:34:28 +0530

    Time: 3 ms (83.42%), Space: 44.5 MB (70.54%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -0,0 +1,36 @@
+class Solution {
+
+  public  static int calculate(char ch){
+        switch(ch){
+            case 'I' :return 1;
+            case 'V' :return 5;
+            case 'X' :return 10;
+            case 'L' :return 50;
+            case 'C' :return 100;
+            case 'D' :return 500;
+            case 'M' :return 1000;
+            default  :return 0;
+        }
+  }
+
+    public int romanToInt(String s) {
+       int n = s.length();
+        int ans =0;
+        int v1,v2;
+        for(int i=0; i<n; i++){
+            v1=calculate(s.charAt(i));
+            if(i<n-1){
+                v2=calculate(s.charAt(i+1));
+                if(v1<v2){
+                    ans -=v1;
+                }else{
+                    ans +=v1;
+                }
+            }else{
+                ans +=v1;
+            }
+        }
+        return ans;
+    }
+}
+
