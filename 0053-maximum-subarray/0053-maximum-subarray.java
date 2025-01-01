c56fbffdfbaa3dbc64c3bb94480c72c52a680f0b
Anurag Tiwari <anuragtiwarigpj2005@gmail.com>
2025-01-01 22:47:54 +0530

    Time: 2 ms (6.18%), Space: 57 MB (51.64%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int maxSubArray(int[] arr) {
+        int currSum =0;
+        int maxSum =Integer.MIN_VALUE;
+        boolean hasPositive = false;
+        int maxNegativeSum = Integer.MIN_VALUE;
+        for(int i=0; i<arr.length; i++){
+             if(arr[i]>0){
+              hasPositive = true;
+             }
+            if(hasPositive){
+              currSum = currSum+arr[i];
+              if(currSum <0){
+                currSum = 0;
+              }  
+              maxSum = Math.max(currSum,maxSum);
+            }
+            if(!hasPositive){
+              maxNegativeSum = Math.max(maxNegativeSum,arr[i]);
+            }
+        }
+        return hasPositive ? maxSum :maxNegativeSum;
+    }
+}
