e9e619cac8f552527d3fd60e51a4073ec177d29b
Anurag Tiwari <anuragtiwarigpj2005@gmail.com>
2025-01-19 23:19:28 +0530

    Time: 0 ms (100%), Space: 41.4 MB (48.62%) - LeetHub

--- 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.java
+++ 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.java
@@ -1,19 +1,5 @@
 class Solution {
     public boolean check(int[] nums) {
-
-      boolean allsame = true;
-      for(int i=1; i<nums.length; i++){
-        if(nums[i-1] != nums[i]){
-            allsame = false;
-        }
-      }
-    //    if(allsame){
-    //     return  true;
-    //    }
-
-
-
-
           int n = nums.length;
           int count =  0;
            for(int i=1; i<nums.length; i++){
@@ -24,7 +10,7 @@ class Solution {
            if(nums[n-1] > nums[0]){
             count ++;
            }
-        //    return count == 1;
-           return allsame ? true : count ==1;
+   
+           return count <=1;
     }
 }
