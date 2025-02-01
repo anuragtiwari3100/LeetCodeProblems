a1b64414df9c5e9afcf6678e688c7a4919f04820
Anurag Tiwari <anuragtiwarigpj2005@gmail.com>
2025-02-01 11:13:28 +0530

    Time: 0 ms (100%), Space: 45 MB (92.1%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,21 +1,13 @@
 class Solution {
-
-
-
-
-      public static int missingNumber(int arr[]) {   //TC => O(n) && SC = O(1)
-        int N = arr.length;    //  we know size  will currSize+1
-        int sum = (N * (N + 1)) / 2;
+      public static int missingNumber(int arr[]) {   
+        int N = arr.length;    
+        long sum = (N * (N + 1)) / 2;
         int tempSum = 0;
         for (int i = 0; i < arr.length; i++) {
             tempSum += arr[i];
         }
-        return sum - tempSum;
+        return (int)sum - tempSum;
     }
-
-
-
-
 }
 
 
