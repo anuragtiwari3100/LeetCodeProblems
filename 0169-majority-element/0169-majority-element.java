f631066e71ce71469a9a1a78282101def34aaa3d
Anurag Tiwari <anuragtiwarigpj2005@gmail.com>
2025-03-25 19:27:12 +0530

    Time: 13 ms (29.72%), Space: 49.4 MB (85.79%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int val :nums){
+            map.put(val,map.getOrDefault(val,0)+1);
+        }
+        int maxFreq =0;
+        int maxFreqElement = -1;
+        for(HashMap.Entry<Integer,Integer> entry :map.entrySet()){
+           if(entry.getValue()>maxFreq){
+            maxFreq =entry.getValue();
+        maxFreqElement=entry.getKey();
+           }
+        }
+        return maxFreqElement; 
+    }
+   
+}
