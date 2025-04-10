7f7ef737f4cf8abd2a10e42a4c45d6aecd93e34e
Anurag Tiwari <anuragtiwarigpj2005@gmail.com>
2025-04-10 17:20:42 +0530

    Time: 4 ms (45.81%), Space: 71.8 MB (97.35%) - LeetHub

--- 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.java
+++ 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.java
@@ -1,25 +1,70 @@
 class Solution {
-    public int[] rearrangeArray(int[] abhishek) {
-       int n= abhishek.length;
+/*
+    public int[] rearrangeArray(int[] arr) {
+       int n= arr.length;
         int pos[] = new int[n/2];
         int neg[] = new int[n/2];
         int p_Couter=0;
         int n_Counter=0;
-        for(int i=0; i<abhishek.length;i++){
-            if(abhishek[i]>0){
-               pos[p_Couter] =abhishek[i] ;
+        for(int i=0; i<arr.length;i++){
+            if(arr[i]>0){
+               pos[p_Couter] =arr[i] ;
                p_Couter++;
             }else{
-               neg[n_Counter] =abhishek[i] ;
+               neg[n_Counter] =arr[i] ;
                n_Counter++;
             }
         }
         
-        for(int i=0; i<abhishek.length/2; i++){
-            abhishek[2*i] = pos[i];
-            abhishek[2*i+1] = neg[i];
+        for(int i=0; i<arr.length/2; i++){
+            arr[2*i] = pos[i];
+            arr[2*i+1] = neg[i];
         }
-        return abhishek;
+        return arr;
+    }
+ */
+
+ /*
+ 
+       public static int[] arrange(int arr[]){
+        ArrayList<Integer> pos= new ArrayList<>();
+        ArrayList<Integer> neg = new ArrayList<>();
+        int resArr[] = new int[arr.length];
+          for(int i=0; i<arr.length; i++){
+              if(arr[i]>0){
+                  pos.add(arr[i]);
+              }else{
+                  neg.add(arr[i]);
+              }
+          }
+          for(int i=0; i<arr.length/2; i++){
+              arr[2*i] = pos.get(i);
+              arr[2*i+1] = neg.get(i);
+          }
+          return arr;
+    }
+  */
+      public int[] rearrangeArray(int[] arr) {
+       int n= arr.length;
+        int pos[] = new int[n/2];
+        int neg[] = new int[n/2];
+        int p_Couter=0;
+        int n_Counter=0;
+        for(int i=0; i<arr.length;i++){
+            if(arr[i]>0){
+               pos[p_Couter] =arr[i] ;
+               p_Couter++;
+            }else{
+               neg[n_Counter] =arr[i] ;
+               n_Counter++;
+            }
+        }
+        
+        for(int i=0; i<arr.length/2; i++){
+            arr[2*i] = pos[i];
+            arr[2*i+1] = neg[i];
+        }
+        return arr;
     }
    
 }
