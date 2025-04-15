be4673d0628e0ab017cdff83c51c91a65d743aa5
Anurag Tiwari <anuragtiwarigpj2005@gmail.com>
2025-04-15 18:25:55 +0530

    Time: 0 ms (100%), Space: 43.2 MB (37.32%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,38 +1,37 @@
 class Solution {
-    public void reverseArr(int arr[], int start , int end){
-    
-           while (start < end) {
-            int temp = arr[start];
-            arr[start] = arr[end];
-            arr[end] = temp;
-            start++;
-            end--;
+    public void revArr(int arr[], int si, int ei){
+        while(si<=ei){
+            int temp= arr[si];
+            arr[si] = arr[ei];
+            arr[ei] = temp;
+            si++;
+            ei--;
         }
-        
     }
     public void nextPermutation(int[] nums) {
-        int n= nums.length;
+        int n = nums.length;
         int index = -1;
-         
-         for(int i=n-2; i>=0; i--){
+
+        for(int i=n-2; i>=0; i--){
             if(nums[i]<nums[i+1]){
                 index =i;
                 break;
             }
-         }
-         if(index == -1){
-            reverseArr(nums,0,n-1);
-            return;
-         }
+        }
 
-         for(int j=n-1; j>=0; j--){
+        if(index == -1){
+            revArr(nums, 0, n-1);
+            return;
+        }
+        for(int j=n-1; j>index; j--){
             if(nums[j]>nums[index]){
-                int temp= nums[j];
-                nums[j] = nums[index];
-                nums[index] =temp;
+                int temp = nums[j];
+                nums[j] =nums[index];
+                nums[index] = temp;
                 break;
             }
-         }
-                 reverseArr(nums,index+1,n-1);
+        }
+        revArr(nums,index+1, n-1);
+        return;
     }
 }
