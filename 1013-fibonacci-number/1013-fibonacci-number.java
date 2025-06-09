f4b683da603eacbd36e85e2193a228c3f4e4da6e
Anurag Tiwari <anuragtiwarigpj2005@gmail.com>
2025-06-09 10:41:48 +0530

    Time: 8 ms (43.82%), Space: 40.5 MB (39.44%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -3,8 +3,8 @@ class Solution {
           if( n <=1){
             return n;
           }
-          int last = fib(n-1);
-          int sec_last = fib(n-2);
-          return last+ sec_last;
+
+           return  fib(n-1)+fib(n-2);
+      
     }
 }
