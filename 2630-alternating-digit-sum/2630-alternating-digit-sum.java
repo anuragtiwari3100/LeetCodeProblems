f0862ca3438500697ea0e3eea2485b88823c9d18
Anurag Tiwari <anuragtiwarigpj2005@gmail.com>
2025-06-14 15:23:01 +0530

    Time: 0 ms (100%), Space: 40.8 MB (14.61%) - LeetHub

--- 2630-alternating-digit-sum/2630-alternating-digit-sum.java
+++ 2630-alternating-digit-sum/2630-alternating-digit-sum.java
@@ -6,11 +6,11 @@ class Solution {
      for(int i=0; i<ch.length; i++ ){//3
         if(i%2 ==0){
         res =res+ Character.getNumericValue(ch[i]);
-        }else{
+        } else{
              res =res - Character.getNumericValue(ch[i]);
 
-        }     
-   
+        }       
+
      }
     return  res;
     }
