182e391c8d124c7446999c67e37a2ba123d0944c
Anurag Tiwari <anuragtiwarigpj2005@gmail.com>
2025-06-14 15:25:32 +0530

    Time: 0 ms (100%), Space: 40.4 MB (61.61%) - LeetHub

--- 2630-alternating-digit-sum/2630-alternating-digit-sum.java
+++ 2630-alternating-digit-sum/2630-alternating-digit-sum.java
@@ -1,7 +1,7 @@
 class Solution {
     public int alternateDigitSum(int n) {
            int res =0;
-     String  str = String.valueOf(n);
+     String  str = Integer.toString(n);
      char[] ch = str.toCharArray();
      for(int i=0; i<ch.length; i++ ){//3
         if(i%2 ==0){
