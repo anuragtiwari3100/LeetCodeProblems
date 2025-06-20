eabdc325dd1e535ca06a5fb90fd80e57b5724157
Anurag Tiwari <anuragtiwarigpj2005@gmail.com>
2025-06-20 15:30:14 +0530

    Time: 12 ms (36.01%), Space: 49.8 MB (77.53%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,77 +1,54 @@
 class Solution {
-    public int majorityElement(int[] nums) {
-/**     //App1
-          HashMap<Integer,Integer> map = new HashMap<>();
-        for(int val :nums){
-            map.put(val,map.getOrDefault(val,0)+1);
+
+/**
+       //only for +ve no
+    public int majorityElement(int[]arr ) {
+           int n = arr.length;
+       int maxEle =arr[0];
+
+       for(int x: arr){
+           maxEle = Math.max(maxEle, x);
+       }
+
+       int res[] = new int[maxEle+1];
+
+    
+    for(int i=0; i<n; i++){
+         res[arr[i]]++;
+    }
+
+
+     int cnt =0;
+     int ans = -1;
+    
+    for(int x=0; x<res.length; x++){
+        if(res[x]>cnt){
+            cnt = res[x];
+            ans = x;
         }
-        int maxFreq =0;
-        int maxFreqElement = -1;
-    for(HashMap.Entry<Integer,Integer> entry:map.entrySet()){
-           if(entry.getValue()>maxFreq){
-            maxFreq =entry.getValue();
-        maxFreqElement=entry.getKey();
-           }
-        }
-        return maxFreqElement; 
+    
+    }
+  return  ans;
+    }
  */
-        /**  App2
-            for(int i=0; i<nums.length; i++){
-        int freqCount =0;
-        for(int j=0; j<nums.length; j++){
-            if(nums[i] == nums[j]){
-            freqCount++; 
+
+    public int majorityElement(int[]arr ) {
+        int  n = arr.length;
+        HashMap<Integer,Integer> map = new  HashMap<>();
+        for(int x : arr){
+            map.put(x,map.getOrDefault(x,0)+1);
+        }
+    
+          int res = arr[0];
+          int  maxFreq = 0;
+        for(int key : map.keySet()){
+            int value = map.get(key);
+            if(value > maxFreq || (value == maxFreq && res < key)){
+                maxFreq = value;
+                res = key;
             }
         }
-        if(freqCount > nums.length/2 ){
-            return nums[i];
-        }
+        return  res;
     }
-        return -1;  
-      */  
-     /*
-         Arrays.sort(nums);
-   int freq =1 ; 
-   int ans = nums[0];
-   for(int i=1; i<nums.length; i++){
-    if(nums[i]== nums[i-1]){
-        freq ++;
-    }else{
-        freq =1;
-        ans = nums[i];
-    }
-    if(freq > nums.length/2){
-        return ans;
-    }
-   }
 
-return nums[0];
-      */
-
-//                          Expwcted  time ans Spac complxity is O(n) is O(n)
-       
-          int freq =0;
-          int ans =0;
-          for(int i=0; i<nums.length; i++){
-            if(freq == 0){
-                ans = nums[i];
-            }
-            if(ans == nums[i]){
-                freq ++;
-            }else{
-                freq --;
-            }
-          }
-
-          int cnt =0;
-          for(int val:nums){
-            if(val == ans){
-                cnt ++;
-            }
-          }
-            if(cnt > nums.length/2) {
-                return ans;
-            }
-   return -1;
-    }
 }
