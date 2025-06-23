a6acba303a9d53c3b638d71da6e40c6813b9310c
Anurag Tiwari <anuragtiwarigpj2005@gmail.com>
2025-06-23 08:51:39 +0530

    Time: 694 ms (24.01%), Space: 59.8 MB (66.06%) - LeetHub

---
+++ 1206-corporate-flight-bookings/1206-corporate-flight-bookings.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] corpFlightBookings(int[][] arr, int n) {
+       int ansArr[] = new int[n];
+    for(int newArr[] : arr){
+        int start  = newArr[0];
+        int end = newArr[1];
+        int seats = newArr[2];
+        
+        for(int i=start-1; i<end; i++){
+           ansArr[i] += seats;
+        }
+    }
+   return  ansArr;
+        
+    }
+}
