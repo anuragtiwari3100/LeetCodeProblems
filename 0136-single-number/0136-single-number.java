f3f7a1beb0accadefc783895b2b72c96608b842a
Anurag Tiwari <anuragtiwarigpj2005@gmail.com>
2025-07-08 13:24:01 +0530

    Time: 1 ms (99.89%), Space: 46.4 MB (35.64%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -18,11 +18,17 @@ class Solution {
     //   return -1;   
 
 
-    int xor =0;
-    for(int i=0; i<arr.length; i++){
-        xor = xor^arr[i];
-    }
-    return xor;  
+    // int xor =0;
+    // for(int i=0; i<arr.length; i++){
+    //     xor = xor^arr[i];
+    // }
+    // return xor;  
+
+   int xor =0;
+   for(int i=0; i<arr.length; i++){
+    xor ^= arr[i];
+   }
+    return xor;
     }
 
 }
